62a8b3fa67bc1ec6db443765dff9981eee527d4f
aman <aman.albughdadi@gmail.com>
2023-10-07 23:20:41 +0300

    0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:ez_orgnize/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
