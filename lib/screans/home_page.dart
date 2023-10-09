07d7057fc3c81c82e68ce1565fb91e4f09525b98
aman <aman.albughdadi@gmail.com>
2023-10-09 13:36:07 +0300

    log in

--- lib/screans/home_page.dart
+++ lib/screans/home_page.dart
@@ -1,14 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class home_page extends StatelessWidget {
+class home_page extends StatefulWidget {
   home_page({super.key});
 
+  @override
+  State<home_page> createState() => _home_pageState();
+}
+
+class _home_pageState extends State<home_page> {
   final user = FirebaseAuth.instance.currentUser;
+  var index =0;
+
 
   void signout() {
     FirebaseAuth.instance.signOut();
   }
+
+  final bottom = [
+    BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
+    BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
+
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +33,13 @@ class home_page extends StatelessWidget {
             icon: Icon(Icons.logout),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottom,
+        currentIndex: index,
+        onTap: (value) => setState(() {
+          index = value;
+        }),
       ),
     );
   }
