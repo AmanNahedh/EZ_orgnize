d43f364f67d90caed2eaf502535788594e9bc666
aman <aman.albughdadi@gmail.com>
2023-10-19 15:35:12 +0300

    add event

--- lib/screans/admin/add_event.dart
+++ lib/screans/admin/add_event.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AddEventPage extends StatelessWidget {
+class AddEventPage extends StatefulWidget {
+  @override
+  _AddEventPageState createState() => _AddEventPageState();
+}
+//note
+class _AddEventPageState extends State<AddEventPage> {
+  DateTime selectedDate = DateTime.now();
+  final _eventNameController = TextEditingController();
+  final _eventLocationController = TextEditingController();
+  final _eventDetailsController = TextEditingController();
+  final _maleOrganizersController = TextEditingController();
+  final _femaleOrganizersController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _eventNameController.dispose();
+    _eventLocationController.dispose();
+    _eventDetailsController.dispose();
+    _maleOrganizersController.dispose();
+    _femaleOrganizersController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,35 +46,100 @@ class AddEventPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Event Name',
-              ),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _eventNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Event Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                GestureDetector(
+                  onTap: () => _selectDate(context),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all()
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.calendar_today),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'Event Date: ${DateFormat('yyyy-MM-dd').format(selectedDate)}',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _eventLocationController,
+                  decoration: InputDecoration(
+                    labelText: 'Event Location',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _eventDetailsController,
+                  decoration: InputDecoration(
+                    labelText: 'Event Details',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    'How many organizers do you need?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.teal,
+                    ),
+                    maxLines: 2,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _maleOrganizersController,
+                        decoration: InputDecoration(
+                          labelText: 'Male',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _femaleOrganizersController,
+                        decoration: InputDecoration(
+                          labelText: 'Female',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add event logic here
+                  },
+                  child: Text('Add Event'),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Event Date',
-              ),
-              keyboardType: TextInputType.datetime,
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Event Location',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add event logic here
-              },
-              child: Text('Add Event'),
-            ),
-          ],
+          ),
         ),
       ),
     );
